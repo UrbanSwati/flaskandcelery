6a1d48083c45af53865c829485ca9945f5fc2319
William Maphanga <williesdk@gmail.com>
2020-01-29 12:51:53 +0200

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -4,6 +4,13 @@ broker_url = 'amqp://myuser:mypassword@localhost:5672/myvhost'
 # List of modules to import when the Celery worker starts.
 imports = ('tasks.tasks',)
 
+# https://coderbook.com/@marcus/how-to-pass-complex-data-types-to-celery-tasks/
+accept_content = ['pickle']
+
+result_serializer = 'pickle'
+
+result_backend = 'rpc://'
+
 task_queues = {
     'background.tasks.add': {
         'exchange': 'background.tasks.add',
