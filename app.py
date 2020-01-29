365126c6dc697913c62443f1f6febefd2563625c
William Maphanga <williesdk@gmail.com>
2020-01-29 12:54:14 +0200

    added answer in return statement

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def do_task():
     first_number = randint(0, 7 ** 7)
     second_number = randint(0, 7 ** 7)
     BackgroundProcessTask().delay(first_number, second_number)
-    return f'{datetime.now()} Adding {first_number} and {second_number}'
+    return f'{datetime.now()} Adding {first_number} and {second_number} = {answer.get()}'
 
 
 if __name__ == '__main__':
